14f78e2bb5423ec9e87d2f531b0a7043ce5d7704
Your Name <jaylenpatterson16@gmail.com>
2022-02-01 14:05:35 +0000

    exercise 1 complete

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,35 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+# user = User.create(name: "David", occupation: "Code Artist")
+
+
+  # * Richmond (annual_revenue of 1260000 carries women's apparel only)
+  # * Gastown (annual_revenue of 190000 carries men's apparel only)
+
+
+burnaby = Store.new(
+  name: "Burnaby", 
+  annual_revenue: 300000, 
+  mens_apparel: true, 
+  womens_apparel: true)
+burnaby.save
+
+
+richmond = Store.new(
+  name: "Richmond",
+  annual_revenue: 1260000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+richmond.save
+
+gastown = Store.new(
+  name: "Gastown",
+  annual_revenue: 190000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+gastown.save
+
+puts Store.count
